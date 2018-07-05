ade9687bcf290b8a5fe99acee05bd40a9fce687c
Shin-ichi Yagi <syagi@cac.co.jp>
2018-07-05 17:07:13 +0900

    Django Debug Toolbar導入

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls import include, url
 import manager.views as manager_view
 
 urlpatterns = [
@@ -23,3 +24,9 @@ urlpatterns = [
     # URLとViewを組み合わせる。
     path('worker_list/', manager_view.WorkerListView.as_view())
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
